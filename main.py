f1566d36dd3f23aa445d09a54355fc8ac60f94b8
MilaSakiew <ludmila.sakiew@gmail.com>
2026-02-16 14:13:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    greetings = [
+        "Deutsch: Hallo World",
+        "English: Hello World",
+        "Français: Bonjour le monde",
+        "Español: Hola Mundo",
+        "Italiano: Ciao Mondo",
+    ]
+
+    for greeting in greetings:
+        print(greeting)
+
+
+if __name__ == "__main__":
+    main()
